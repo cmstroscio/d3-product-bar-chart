512ca33dfbd559e0d5cfa5cb45834e498928bf9e
marshall <marshals@netapp.com>
2017-09-03 11:42:34 -0400

    add big chart, fas

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('browsersync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+            port: 3000
+        }
+    });
+
+    gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch("./*.js").on('change', browserSync.reload);
+});
